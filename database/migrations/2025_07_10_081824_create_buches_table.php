2cb3215aa8e77e36958f3446e883dd9dc64d5dff
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-10 09:09:49 +0000

    Die Spalte(title/autor) erstllen  / Create Culomn title and author

--- database/migrations/2025_07_10_081824_create_buches_table.php
+++ database/migrations/2025_07_10_081824_create_buches_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('buches', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('autor');
             $table->timestamps();
         });
     }
