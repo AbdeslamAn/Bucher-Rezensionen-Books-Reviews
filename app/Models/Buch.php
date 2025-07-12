1c9d219725e5a1dc2c68dbdb46faac7d4bdd00ce
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-12 06:58:59 +0000

    Definition der Beziehung im Buch-Modell (hasMany) / Definition of the Relationship in the Book Model (hasMany)

--- app/Models/Buch.php
+++ app/Models/Buch.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Buch extends Model
 {
     use HasFactory;
+
+    public function bewertung()
+    {
+        return $this->hasMany(Bewertung::class);
+    }
 }
