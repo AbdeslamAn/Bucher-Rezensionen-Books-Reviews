12031422d9025b0dda348511703aae6d381bf50a
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-13 08:27:55 +0000

    Benenne ich RezentionFactory.php in RezensionFactory.php um. und fakedaten erstellen /Rename RezentionFactory.php to RezensionFactory.php and create fake data

---
+++ database/factories/RezensionFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rezension>
+ */
+class RezensionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'buch_id' => null,
+            'rezension' => fake()->paragraph,
+            'bewertung' => fake()->numberBetween(1, 5),
+            'created_at' => fake()->dateTimeBetween('-2 years'),
+            'updated_at' => function (array $attributes)
+            {
+                return fake()->dateTimeBetween($attributes['created_at']);
+            }
+        ];
+
+
+    }
+    public function gut()
+    {
+        return $this->state(function (array $attributes)
+        {
+            return[
+                'bewertung' => fake()->numberBetween(4, 5)
+            ];
+        });
+    }
+
+    public function averege()
+    {
+        return $this->state(function (array $attributes)
+        {
+            return[
+                'bewertung' => fake()->numberBetween(2, 5)
+            ];
+        });
+    }
+
+    public function schlecht()
+    {
+        return $this->state(function (array $attributes)
+        {
+            return[
+                'bewertung' => fake()->numberBetween(1, 3)
+            ];
+        });
+    }
+}
