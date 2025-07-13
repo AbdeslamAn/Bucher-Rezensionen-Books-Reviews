b37df654c6825ff05a90eb1e707fdb0e6951e92e
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-13 08:37:25 +0000

    Fakedaten generieren / generate   fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Buch;
+use App\Models\Rezension;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,39 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Buch::factory(33)->create()->each(function ($buch)
+        {
+            $numRezension = random_int(5, 30);
+
+            Rezension::factory()->count($numRezension)
+                ->gut()
+                ->for($buch)
+                ->create();
+        });
+
+        Buch::factory(33)->create()->each(function ($buch)
+        {
+            $numRezension = random_int(5, 30);
+
+            Rezension::factory()->count($numRezension)
+                ->average()
+                ->for($buch)
+                ->create();
+        });
+
+        Buch::factory(34)->create()->each(function ($buch)
+        {
+            $numRezension = random_int(5, 30);
+
+            Rezension::factory()->count($numRezension)
+                ->schlecht()
+                ->for($buch)
+                ->create();
+        });
     }
 }
