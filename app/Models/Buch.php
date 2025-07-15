7c138e69d7164e45797c46fcfdf9481712e974ad
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-15 16:24:29 +0000

    Lokalen Abfragebereich für die Titelsuche von Büchern hinzugefügt / Lokalen Abfragebereich für die Titelsuche von Büchern hinzugefügt

--- app/Models/Buch.php
+++ app/Models/Buch.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
@@ -12,5 +14,10 @@ class Buch extends Model
     public function rezension()
     {
         return $this->hasMany(Rezension::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
