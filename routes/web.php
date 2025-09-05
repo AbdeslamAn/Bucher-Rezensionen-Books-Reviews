9ee62a9b10622e734999ba1b00b0465ed10737dd
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-09-05 17:50:34 +0000

    Route::resource estellen / Create Route::resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BuchController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('buchs', BuchController::class);
