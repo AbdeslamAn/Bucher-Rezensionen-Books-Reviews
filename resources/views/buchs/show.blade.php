110cdf3b7fa67c1a31db1bd4d25895f6ad687b01
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-09-29 11:08:59 +0100

    Die SeiteInhalt show.blade.php  hinzufügen / Add the content show.blade.php

---
+++ resources/views/buchs/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="mb-4">
+    <h1 class="mb-2 text-2xl text-center underline">{{ $buch->title }}</h1>
+
+    <div class="book-info">
+      <div class="book-author text-center mb-4 text-lg font-semibold">von {{ $buch->autor }}</div>
+      <div class="book-rating flex items-center">
+        <div class="mr-2 text-sm font-medium text-slate-700 ">
+          {{ number_format($buch->rezension_avg_bewertung, 1) }}
+        </div>
+        <span class="text-center book-review-count  text-sm text-gray-500 ">
+          {{ $buch->rezension_count }} {{ Str::plural('rezension', $buch->rezension_count) }}
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <h2 class="mb-4 text-xl font-semibold text-center">Rezension</h2>
+    <ul>
+      @forelse ($buch->rezension as $rezension)
+        <li class="book-item mb-4">
+          <div>
+            <div class="mb-2 flex items-center justify-between">
+              <div class="font-semibold">{{ $rezension->bewertung }}</div>
+              <div class="book-review-count">
+                {{ $rezension->created_at->format('M j, Y') }}</div>
+            </div>
+            <p class="text-gray-700">{{ $rezension->rezension }}</p>
+          </div>
+        </li>
+      @empty
+        <li class="mb-4">
+          <div class="empty-book-item">
+            <p class="empty-text text-lg font-semibold">Noch keine Rezension.</p>
+          </div>
+        </li>
+      @endforelse
+    </ul>
+  </div>
+@endsection
