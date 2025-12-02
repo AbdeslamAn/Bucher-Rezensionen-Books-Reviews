56c0bc807cd16dbd506eaafa7f7b9aba12a446b9
AbdeslamAn <abdeslamanssar@gmail.com>
2025-12-02 08:59:17 +0100

    Form erstellen für Datei: create.blade.php / Add From for File create.blade.php

---
+++ resources/views/buchs/rezensions/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="mb-10 text-2xl">Rezension erstellen für {{ $buch->title }}</h1>
+
+    <form method="POST" action="{{ route('buchs.rezension.store', $buch) }}">
+        @csrf
+        <label for="rezension">Rezension</label>
+        <textarea name="rezension" id="rezension" required class="input mb-4"></textarea>
+
+        <label for="bewertung">Bewertung</label>
+
+        <select name="bewertung" id="bewertung" class="input mb-4" required>
+            <option value="">Bitte eine Bewertung auswählen</option>
+            @for ($i = 1; $i < 5; $i++)
+                <option value="{{ $i }}">{{ $i }}</option>
+            @endfor
+        </select>
+
+        <button type="submit" class="btn">Rezension erstellen</button>
+    </form>
