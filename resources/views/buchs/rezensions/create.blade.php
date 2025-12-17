5cb06c9b65f3a65fffc254beb424ec673d8afa4a
AbdeslamAn <abdeslamanssar@gmail.com>
2025-12-17 08:32:11 +0100

    center div + label float-left

--- resources/views/buchs/rezensions/create.blade.php
+++ resources/views/buchs/rezensions/create.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<center>
     <div class=" w-[400px] flex flex-col md:w-full">
     <h1 class="mb-10 text-2xl flex justify-center ">Rezension erstellen für {{ $buch->title }}</h1>
 
     <form method="POST" action="{{ route('buchs.rezensions.store', $buch) }}">
         @csrf
-        <label for="rezension">Rezension</label>
+        <label for="rezension" class=" float-left">Rezension</label>
         <textarea name="rezension" id="rezension" required class="input mb-4"></textarea>
 
-        <label for="bewertung">Bewertung</label>
+        <label for="bewertung" class=" float-left">Bewertung</label>
 
         <select name="bewertung" id="bewertung" class="input mb-4" required>
             <option value="">Bitte eine Bewertung auswählen</option>
@@ -18,6 +19,7 @@
             @endfor
         </select>
 
-        <button type="submit" class="createbtn">Rezension erstellen</button>
+        <button type="submit" class="createbtn float-left">Rezension erstellen</button>
     </form>
     </div>
+</center>
